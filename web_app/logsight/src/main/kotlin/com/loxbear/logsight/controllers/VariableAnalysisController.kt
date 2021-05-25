a8f1a6fc833174c670fcde87ac58228a25ae11b3
grozdan <grozdan.madzarovamg@gmail.com>
2021-05-25 23:58:51 +0200

    multiple improvements points 12, 13, 19

--- web_app/logsight/src/main/kotlin/com/loxbear/logsight/controllers/VariableAnalysisController.kt
+++ web_app/logsight/src/main/kotlin/com/loxbear/logsight/controllers/VariableAnalysisController.kt
@@ -17,32 +17,34 @@ class VariableAnalysisController(val variableAnalysisService: VariableAnalysisSe
                                  val usersService: UsersService, val applicationService: ApplicationService) {
 
     @GetMapping("/application/{id}")
-    fun getTemplates(@PathVariable id: Long, @RequestParam(required = false) search: String?, authentication: Authentication): List<VariableAnalysisHit> {
+    fun getTemplates(@PathVariable id: Long,
+                     @RequestParam(required = false) search: String?,
+                     @RequestParam startTime: String,
+                     @RequestParam endTime: String,
+                     authentication: Authentication): List<VariableAnalysisHit> {
         val user = usersService.findByEmail(authentication.name)
         val application = applicationService.findById(id)
         val applicationsIndexes = variableAnalysisService.getApplicationIndex(application, user.key)
-        val startTime = "now-12h"
-        val stopTime = "now"
-        return variableAnalysisService.getTemplates(applicationsIndexes, startTime, stopTime, search)
+        return variableAnalysisService.getTemplates(applicationsIndexes, startTime, endTime, search, user)
     }
 
     @PostMapping("/application/{id}/specific_template")
     fun getSpecificTemplate(@PathVariable id: Long,
                             @RequestBody specificTemplate: SpecificTemplateRequest,
+                            @RequestParam startTime: String,
+                            @RequestParam endTime: String,
                             authentication: Authentication): Pair<String, List<LineChart>> {
         val user = usersService.findByEmail(authentication.name)
         val application = applicationService.findById(id)
         val applicationsIndexes = variableAnalysisService.getApplicationIndex(application, user.key)
-        val startTime = "now-12h"
-        val stopTime = "now"
         with(specificTemplate) {
-            val tmp = variableAnalysisService.getSpecificTemplateGrouped(applicationsIndexes, startTime, stopTime, template, param, paramValue)
-            return variableAnalysisService.getSpecificTemplateGrouped(applicationsIndexes, startTime, stopTime, template, param, paramValue)
+            return variableAnalysisService.getSpecificTemplateGrouped(applicationsIndexes, startTime, endTime, template, param, paramValue)
         }
     }
 
     @GetMapping("/application/{id}/top_n_templates")
-    fun getTop5Templates(@PathVariable id: Long, authentication: Authentication): Map<String, List<TopNTemplatesData>> {
+    fun getTop5Templates(@PathVariable id: Long,
+                         authentication: Authentication): Map<String, List<TopNTemplatesData>> {
         val user = usersService.findByEmail(authentication.name)
         val application = applicationService.findById(id)
         val applicationsIndexes = variableAnalysisService.getApplicationIndex(application, user.key)
@@ -50,13 +52,14 @@ class VariableAnalysisController(val variableAnalysisService: VariableAnalysisSe
     }
 
     @GetMapping("/application/{id}/log_count_line_chart")
-    fun getLogCountLineChart(@PathVariable id: Long, authentication: Authentication): List<LineChart> {
+    fun getLogCountLineChart(@PathVariable id: Long,
+                             @RequestParam startTime: String,
+                             @RequestParam endTime: String,
+                             authentication: Authentication): List<LineChart> {
         val user = usersService.findByEmail(authentication.name)
         val application = applicationService.findById(id)
         val applicationsIndexes = variableAnalysisService.getApplicationIndex(application, user.key)
-        val startTime = "now-12h"
-        val stopTime = "now"
-        return variableAnalysisService.getLogCountLineChart(applicationsIndexes, startTime, stopTime)
+        return variableAnalysisService.getLogCountLineChart(applicationsIndexes, startTime, endTime)
     }
 
 }
