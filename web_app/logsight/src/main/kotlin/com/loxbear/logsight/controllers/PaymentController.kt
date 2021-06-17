cb19cb5fa7021e64f10020009b53056f21661a35
grozdan <grozdan.madzarovamg@gmail.com>
2021-06-17 10:23:59 +0200

    add loggers for payment

--- web_app/logsight/src/main/kotlin/com/loxbear/logsight/controllers/PaymentController.kt
+++ web_app/logsight/src/main/kotlin/com/loxbear/logsight/controllers/PaymentController.kt
@@ -2,12 +2,14 @@ package com.loxbear.logsight.controllers
 
 import com.google.gson.Gson
 import com.loxbear.logsight.models.CheckoutPayment
+import com.loxbear.logsight.services.UsersService
 import com.stripe.Stripe
 import com.stripe.exception.SignatureVerificationException
 import com.stripe.exception.StripeException
 import com.stripe.model.checkout.Session
 import com.stripe.net.Webhook
 import com.stripe.param.checkout.SessionCreateParams
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.*
 import javax.servlet.http.HttpServletRequest
 
@@ -15,7 +17,7 @@ import javax.servlet.http.HttpServletRequest
 @RestController
 @RequestMapping("/api/payments")
 class PaymentController {
-
+    val logger = LoggerFactory.getLogger(PaymentController::class.java)
     private val gson = Gson()
 
     fun init() {
@@ -26,6 +28,7 @@ class PaymentController {
     @Throws(StripeException::class)
     @PostMapping
     fun paymentWithCheckoutPage(@RequestBody payment: CheckoutPayment): String? {
+        logger.info("Payment subscription [{}]", payment)
         init()
         val params: SessionCreateParams = SessionCreateParams.builder() // We will use the credit card payment method
             .addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD)
@@ -47,6 +50,7 @@ class PaymentController {
 
     @PostMapping("/webhook")
     fun webhook(request: HttpServletRequest, @RequestBody json: String): String? {
+        logger.info("Webhook [{}]", json)
         val sigHeader: String = request.getHeader("Stripe-Signature")
         val endpointSecret: String = "STRIPE_WEBHOOK_SECRET"
 
