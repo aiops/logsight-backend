78598645f6f8ae7a593d04df5dfd3c6fe6b8ee9b
snedelkoski <sashonedelkoski@gmail.com>
2021-06-28 11:51:47 +0200

    kibana autologin problem solved, and provided information fields for every chart/table.

--- web_app/logsight/src/main/kotlin/com/loxbear/logsight/controllers/ApplicationController.kt
+++ web_app/logsight/src/main/kotlin/com/loxbear/logsight/controllers/ApplicationController.kt
@@ -4,8 +4,13 @@ import com.loxbear.logsight.entities.Application
 import com.loxbear.logsight.models.ApplicationRequest
 import com.loxbear.logsight.services.ApplicationService
 import com.loxbear.logsight.services.UsersService
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.http.ResponseEntity
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.client.postForEntity
+import utils.UtilsService.Companion.createKibanaRequestWithHeaders
 
 
 @RestController
@@ -14,11 +19,25 @@ class ApplicationController(
     val applicationService: ApplicationService,
     val usersService: UsersService
 ) {
+    val restTemplate = RestTemplateBuilder()
+        .basicAuthentication("elastic", "elasticsearchpassword")
+        .build();
+
+    @Value("\${kibana.url}")
+    private val kibanaUrl: String? = null
 
     @PostMapping
     fun createApplication(@RequestBody body: ApplicationRequest): Application {
         val user = usersService.findByKey(body.key)
         return applicationService.createApplication(body.name, user)
+    }
+
+
+    @PostMapping("/kibana/login")
+    fun kibanaLogin(@RequestBody requestBody: String): ResponseEntity<String> {
+        val request = createKibanaRequestWithHeaders(requestBody)
+        val response = restTemplate.postForEntity<String>("http://localhost:5601/kibana/api/security/v1/login", request)
+        return response
     }
 
     @GetMapping("/user/{key}")
