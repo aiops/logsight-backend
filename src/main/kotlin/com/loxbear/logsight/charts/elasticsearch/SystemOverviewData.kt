a42156799f3138d2a9da1699f6ea9f915530c60e
Alexander Acker <alexander.acker@tu-berlin.de>
2021-08-17 10:03:32 +0200

    Removed unnecessary imports.

--- src/main/kotlin/com/loxbear/logsight/charts/elasticsearch/SystemOverviewData.kt
+++ src/main/kotlin/com/loxbear/logsight/charts/elasticsearch/SystemOverviewData.kt
@@ -1,7 +1,6 @@
 package com.loxbear.logsight.charts.elasticsearch
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import com.loxbear.logsight.charts.elasticsearch.Aggregations
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class SystemOverviewData(val aggregations: HeatMapAggregations)
