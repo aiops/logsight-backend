920ec18d58834d8f5cc40b8e8415bd2adc7a3a3d
snedelkoski <sashonedelkoski@gmail.com>
2021-09-13 12:44:54 +0200

    payment improvement, the pipeline needs to be tested

--- src/main/kotlin/com/loxbear/logsight/services/KafkaService.kt
+++ src/main/kotlin/com/loxbear/logsight/services/KafkaService.kt
@@ -3,13 +3,29 @@ package com.loxbear.logsight.services
 import com.loxbear.logsight.entities.Application
 import com.loxbear.logsight.entities.LogsightUser
 import com.loxbear.logsight.entities.enums.ApplicationAction
+import com.loxbear.logsight.models.auth.Email
+import com.loxbear.logsight.repositories.UserRepository
 import org.json.JSONObject
+import org.slf4j.LoggerFactory
+import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
+import org.thymeleaf.TemplateEngine
+import org.thymeleaf.context.Context
+import java.net.URL
 
 
 @Service
-class KafkaService(val kafkaTemplate: KafkaTemplate<String, String>) {
+class KafkaService(val kafkaTemplate: KafkaTemplate<String, String>,
+                   val userService: UserService,
+                    val paymentService: PaymentService,
+                    val userRepository: UserRepository,
+                    val emailService: EmailService,
+                   val templateEngine: TemplateEngine) {
+    val logger = LoggerFactory.getLogger(KafkaService::class.java)
+    val exceededMailSubject = "Logsight.ai Limit exceeded"
+    val nearlyExceededMailSubject = "Logsight.ai Limit at 80%"
+
     fun applicationChange(application: Application, action: ApplicationAction) {
         with(application) {
             val message = JSONObject().put("private_key", user.key.toLowerCase().filter { it.isLetterOrDigit() })
@@ -36,6 +52,54 @@ class KafkaService(val kafkaTemplate: KafkaTemplate<String, String>) {
 //        kafkaTemplate.send("${keyApplicationId}_log_compare", message.toString())
     }
 
+    @KafkaListener(topics= ["application_stats"], groupId = "")
+    fun consume(message:String) :Unit {
+        val privateKey = JSONObject(message).getString("private_key")
+        val usedDataNow = JSONObject(message).getLong("quantity")
+        logger.info("Received application user stats update message: [{}]", message)
+        val user = userService.findByKey(privateKey)
+        val usedDataPrevious = user.usedData
+        val availableData = user.availableData
+        if ((usedDataPrevious + usedDataNow) > availableData){
+            paymentService.updateHasPaid(user, false)
+            emailService.sendMimeEmail(
+                Email(
+                    mailTo = user.email,
+                    sub = exceededMailSubject,
+                    body = getExceededLimitsMailBody(
+                        "exceededLimits",
+                        exceededMailSubject)
+                    )
+                )
+        }
+
+        if ((usedDataPrevious + usedDataNow) > 0.8*availableData){
+            emailService.sendMimeEmail(
+                Email(
+                    mailTo = user.email,
+                    sub = nearlyExceededMailSubject,
+                    body = getExceededLimitsMailBody(
+                        "nearlyExceededLimits",
+                        nearlyExceededMailSubject)
+                )
+            )
+        }
+
+        userRepository.updateUsedData(user.key, usedDataPrevious + usedDataNow)
+
+    }
+
+
+    private fun getExceededLimitsMailBody(
+        template: String,
+        title: String,
+    ): String = templateEngine.process(
+        template,
+        with(Context()) {
+            setVariable("title", title)
+            this
+        }
+    )
     //private_key + '_application_stats'
     //quantity
     //check if the quantity is over the limit
