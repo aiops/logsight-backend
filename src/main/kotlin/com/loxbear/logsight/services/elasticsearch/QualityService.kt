e95b0342200e55e2b1ec72a20ed17c8501a773cd
snedelkoski <sashonedelkoski@gmail.com>
2021-09-16 13:49:10 +0200

    bugfix log quality endpoint

--- src/main/kotlin/com/loxbear/logsight/services/elasticsearch/QualityService.kt
+++ src/main/kotlin/com/loxbear/logsight/services/elasticsearch/QualityService.kt
@@ -149,7 +149,7 @@ class QualityService(val repository: QualityRepository, val applicationService: 
 
             try {
                 restTemplate.postForEntity<String>(
-                    "http://localhost:5444/api_v1/results", HttpEntity(json.toString(), headers)).body!!
+                    "http://demo.logsight.ai/api_v1/results", HttpEntity(json.toString(), headers)).body!!
             }catch (e: HttpClientErrorException){
                 return HttpStatus.BAD_REQUEST
             }
