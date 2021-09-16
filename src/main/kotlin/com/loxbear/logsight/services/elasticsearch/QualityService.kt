3c9a45a3297e59a0382de9f2cd52bf8511a66a6d
snedelkoski <sashonedelkoski@gmail.com>
2021-09-16 14:26:21 +0200

    fix

--- src/main/kotlin/com/loxbear/logsight/services/elasticsearch/QualityService.kt
+++ src/main/kotlin/com/loxbear/logsight/services/elasticsearch/QualityService.kt
@@ -149,7 +149,7 @@ class QualityService(val repository: QualityRepository, val applicationService: 
 
             try {
                 restTemplate.postForEntity<String>(
-                    "https://demo.logsight.ai/api_v1/results", HttpEntity(json.toString(), headers)).body!!
+                    "http://data-input-rest-api:5444/api_v1/results", HttpEntity(json.toString(), headers)).body!!
             }catch (e: HttpClientErrorException){
                 return HttpStatus.BAD_REQUEST
             }
