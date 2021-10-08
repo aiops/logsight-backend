bc1e4eaef1f0f15c4139c3891a3bd765023b329c
snedelkoski <sashonedelkoski@gmail.com>
2021-10-08 23:20:26 +0200

    testing payment email

--- src/main/kotlin/com/loxbear/logsight/controllers/PaymentController.kt
+++ src/main/kotlin/com/loxbear/logsight/controllers/PaymentController.kt
@@ -140,10 +140,12 @@ class PaymentController(
                 val quantity = JSONObject(data.toString()).getLong("quantity")
                 logger.info("Quantity is successfully obtained from the stripe webhook")
                 logger.info(quantity.toString())
+                var availableData = 0L
                 if (!user.hasPaid){
-                    paymentService.resetAvailableAndUsedData(user)
+                    availableData = quantity*1000000000
+                }else{
+                    availableData = quantity*1000000000 + user.availableData
                 }
-                val availableData = quantity*1000000000 + user.availableData
                 logger.info("The previously availableData of the user [{}] was [{}] B, now it is updated to [{}] B", user.availableData.toString(), availableData.toString())
                 paymentService.paymentSuccessful(user, customerId, availableData)
                 logger.info("The database was updated with the newest details for the available data for the user with ID: [{}]", user.id)
