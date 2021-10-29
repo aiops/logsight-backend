bc0d985a8f84b68e265540a1fe98985fe08d7f43
Alexander Acker <alexander.acker@tu-berlin.de>
2021-10-29 18:04:35 +0200

    Reworked file upload. (#34)

--- src/main/kotlin/com/loxbear/logsight/entities/enums/LogFileTypes.kt
+++ src/main/kotlin/com/loxbear/logsight/entities/enums/LogFileTypes.kt
@@ -3,5 +3,5 @@ package com.loxbear.logsight.entities.enums
 enum class LogFileTypes(val frontEndDescriptor: String) {
     LOGSIGHT_JSON("JSON - native"),
     SYSLOG("Syslog"),
-    LOGSTASH_JSON("JSON - logstash");
+    UNKNOWN_FORMAT("Other")
 }
