349db2417a16746d92aef8ad27d6e28665478ba5
snedelkoski <sashonedelkoski@gmail.com>
2022-01-12 18:46:55 +0100

    add depenndecy security

---
+++ src/main/kotlin/ai/logsight/backend/security/authentication/AuthService.kt
@@ -0,0 +1,7 @@
+package ai.logsight.backend.security.authentication
+
+interface AuthService {
+    // define all methods
+//    fun generateAuthenticationToken()
+//    fun authenticateUser()
+}
---
+++ src/main/kotlin/ai/logsight/backend/security/authentication/AuthServiceImpl.kt
@@ -0,0 +1,7 @@
+package ai.logsight.backend.security.authentication
+
+import org.springframework.security.authentication.AuthenticationManager
+
+class AuthServiceImpl (authenticationManager: AuthenticationManager): AuthService{
+
+}
