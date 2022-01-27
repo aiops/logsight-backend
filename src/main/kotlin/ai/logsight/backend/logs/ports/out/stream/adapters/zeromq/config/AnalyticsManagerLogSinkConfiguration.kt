aff3846eee279ec13bc8d308c15c6543f6ad8111
snedelkoski <sashonedelkoski@gmail.com>
2022-01-27 14:16:18 +0100

    zeromq fixing

--- src/main/kotlin/ai/logsight/backend/logs/ports/out/stream/adapters/zeromq/config/AnalyticsManagerLogSinkConfiguration.kt
+++ src/main/kotlin/ai/logsight/backend/logs/ports/out/stream/adapters/zeromq/config/AnalyticsManagerLogSinkConfiguration.kt
@@ -11,9 +11,8 @@ class AnalyticsManagerLogSinkConfiguration(
     @Bean
     fun zeroMqPubSocket(): ZMQ.Socket {
         val context = ZMQ.context(1)
-        val socket = context.socket(SocketType.PUB)
-        val connected = socket.connect("${logSinkConfig.protocol}://${logSinkConfig.host}:${logSinkConfig.port}")
-        println(connected)
+        val socket = context.socket(SocketType.PUSH)
+        socket.bind("${logSinkConfig.protocol}://${logSinkConfig.host}:${logSinkConfig.port}")
         return socket
     }
 }
