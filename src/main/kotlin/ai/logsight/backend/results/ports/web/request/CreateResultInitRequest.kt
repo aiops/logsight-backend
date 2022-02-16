86504f886b50ca775e73a5cf9e60a7fa95dde50c
snedelkoski <sashonedelkoski@gmail.com>
2022-02-16 10:43:00 +0100

    refactor the result api

--- src/main/kotlin/ai/logsight/backend/results/ports/web/request/CreateResultInitRequest.kt
+++ src/main/kotlin/ai/logsight/backend/results/ports/web/request/CreateResultInitRequest.kt
@@ -1,11 +1,11 @@
 package ai.logsight.backend.results.ports.web.request
 
+import ai.logsight.backend.results.domain.ResultOperations
 import java.util.*
-import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.NotNull
-import javax.validation.constraints.Pattern
 
 data class CreateResultInitRequest(
     @get:NotNull(message = "receiptId must not be empty")
-    val receiptId: UUID
+    val receiptId: UUID,
+    val operation: ResultOperations
 )
