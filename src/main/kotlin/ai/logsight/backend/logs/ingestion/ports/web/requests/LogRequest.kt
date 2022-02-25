b59e427a4e98da7c2e2c0a32be792a313b949c76
snedelkoski <sashonedelkoski@gmail.com>
2022-02-25 12:12:11 +0100

    adding pattern for timestamp

--- src/main/kotlin/ai/logsight/backend/logs/ingestion/ports/web/requests/LogRequest.kt
+++ src/main/kotlin/ai/logsight/backend/logs/ingestion/ports/web/requests/LogRequest.kt
@@ -1,11 +1,20 @@
 package ai.logsight.backend.logs.ingestion.ports.web.requests
 
 import com.fasterxml.jackson.annotation.JsonInclude
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.Pattern
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class LogRequest(
+
+    @get:Pattern(
+        regexp = "(\\d{4}-\\d{2}-\\d{2}[A-Z]+\\d{2}:\\d{2}:\\d{2}.[0-9+-:]+)",
+        message = "timestamp must be defined as ISO timestamp " +
+            "YYYY-MM-DDTHH:mm:ss.SSSSSS+HH:00. If timezone is not specified, UTC is default."
+    )
     val timestamp: String?,
     val message: String,
+    @get:Pattern(regexp = "INFO|WARNING|WARN|FINE|FINER|DEBUG|ERR|ERROR|EXCEPTION|SEVERE", message = "level must be one of INFO|WARNING|WARN|FINE|FINER|DEBUG|ERR|ERROR|EXCEPTION|SEVERE")
     val level: String?,
     val metadata: String?,
 )
