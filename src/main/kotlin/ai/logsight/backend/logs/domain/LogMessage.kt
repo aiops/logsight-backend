fd59b7602c5955ecc9c06efacdad64947c260fe9
Sasho Nedelkoski <sashonedelkoski@gmail.com>
2022-03-14 09:43:58 +0100

    timestamp verification (#56)

--- src/main/kotlin/ai/logsight/backend/logs/domain/LogMessage.kt
+++ src/main/kotlin/ai/logsight/backend/logs/domain/LogMessage.kt
@@ -7,7 +7,7 @@ import javax.validation.constraints.Pattern
 data class LogMessage(
 
     @get:Pattern(
-        regexp = "(\\d{4}-\\d{2}-\\d{2}[A-Z]+\\d{2}:\\d{2}:\\d{2}.[0-9+-:]+)",
+        regexp = "(\\d{4}-\\d{2}-\\d{2}[A-Z]+\\d{2}:\\d{2}:\\d{2}.[0-9+-:Z]+)",
         message = "timestamp must be defined as ISO timestamp " +
             "YYYY-MM-DDTHH:mm:ss.SSSSSS+HH:00. If timezone is not specified, UTC is default."
     )
