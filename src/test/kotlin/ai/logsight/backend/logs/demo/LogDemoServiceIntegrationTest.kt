da7284daeca9a228cc0e0deb11c0407d9e991679
Alexander Acker <alexander.acker@tu-berlin.de>
2022-06-13 16:01:01 +0200

    Fix kafka tests (#105)
    * Fixed kafka tests to use kafka connector by setting the environment variable (making sure the kafka bean is created). 
    * Removed deprectated test configs.
    * fixed service demo test

---
+++ src/test/kotlin/ai/logsight/backend/logs/demo/LogDemoServiceIntegrationTest.kt
@@ -0,0 +1,44 @@
+package ai.logsight.backend.logs.demo
+
+import ai.logsight.backend.TestInputConfig
+import ai.logsight.backend.TestInputConfig.baseUser
+import ai.logsight.backend.users.ports.out.persistence.UserRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.ActiveProfiles
+import org.junit.jupiter.api.*
+
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@SpringBootTest
+@DirtiesContext
+internal class LogDemoServiceIntegrationTest {
+
+    @Autowired
+    private lateinit var userRepository: UserRepository
+
+    @Autowired
+    lateinit var demoService: LogDemoService
+
+    @Nested
+    @DisplayName("hadoop demo data")
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    inner class LoadDemoData {
+        @BeforeAll
+        fun setUp() {
+            userRepository.deleteAll()
+            userRepository.save(TestInputConfig.baseUserEntity)
+        }
+
+        @Test
+        fun createHadoopDemoForUser() {
+            // given
+            val user = baseUser
+            // when
+            demoService.createHadoopDemoForUser(user)
+            // then
+        }
+    }
+}
