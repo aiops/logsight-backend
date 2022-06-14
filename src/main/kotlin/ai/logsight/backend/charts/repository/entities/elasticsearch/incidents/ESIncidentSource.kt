1bd2eedd679d27b0489887b6d974cf34d6d3759f
Alexander Acker <alexander.acker@tu-berlin.de>
2022-06-14 10:15:12 +0200

    fixed json properties to param, i.e. considering them for setters but not for getters

--- src/main/kotlin/ai/logsight/backend/charts/repository/entities/elasticsearch/incidents/ESIncidentSource.kt
+++ src/main/kotlin/ai/logsight/backend/charts/repository/entities/elasticsearch/incidents/ESIncidentSource.kt
@@ -7,14 +7,14 @@ import com.fasterxml.jackson.annotation.JsonProperty
 data class ESIncidentSource(
     val timestamp: String,
     val risk: Long,
-    @JsonProperty("count_messages")
+    @param:JsonProperty("count_messages")
     val countMessages: Long,
-    @JsonProperty("count_states")
+    @param:JsonProperty("count_states")
     val countStates: Long,
     val status: Long,
-    @JsonProperty("added_states")
+    @param:JsonProperty("added_states")
     val countAddedState: Long,
-    @JsonProperty("level_faults")
+    @param:JsonProperty("level_faults")
     val countLevelFault: Long,
     val severity: Long,
     val tags: Map<String, String>,
